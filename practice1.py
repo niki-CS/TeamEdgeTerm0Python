3ae4d1407ea3a4f1fc0e11fd7a535452e5303dfa
niki-CS <nicole.gil@csedge.org>
2022-07-07 11:11:52 -0400

    lab 1 challenges

---
+++ practice1.py
@@ -0,0 +1,11 @@
+age = 17
+
+name = 'Nicole'
+
+print('Hello World')
+print(age)
+
+#This prints my age and name the f means format, this must be included for it to work
+print(f'My name is {name} and I am {age} years old.')
+
+#Create a new variable and print it
--- variables.py
+++ variables.py
@@ -18,33 +18,40 @@ print(message)
 
 print("\n------------------- Challenge 1 -------------------\n")
 
-#print(f"Hi! My name is {name}")
-#print(f"I'm in the {grade}th grade.")
-#print(f"A fun fact about me is that {funFact}")
-
-
 # **** Challenge 1: Problem 1 ****
 # Initialize the variables used in the {} with your data for the three statements above! 
 # Hint: Also think about where you would initialize those variables
 
+name = 'Nicole'
+
+grade = 12
+
+funFact = "I have 2 older sisters"
+
+print(f'Hi! My name is {name}.')
+print(f"I'm in the {grade}th grade.")
+print(f"A fun fact about me is that {funFact}.")
 
 # **** Upchallenge! ****
 # Can you also print out the data type of the variables? 
-
-
-
+print(type(name))
+print(type(grade))
+print(type(funFact))
 
 # **** Challenge 1: Problem 2 ****
 # Create a string for the different lines of the Happy Birthday Song
 # Print out the song using the strings you declared.
+
+print("Happy Birthday to you.\nHappy Birthday to you.\nHappy Birthday dear Nicole.\nHappy Birthday to you.")
+
 
 
 
 
 # **** Challenge 1: Problem 3 ****
 # Print out a string that has brackets in it.
-
-
+birthday = 'April 20th'
+print(f'My birthday is on {birthday}')
 
 
 # -------------------------------------------- 
@@ -62,38 +69,41 @@ num2 = 10
 # **** Challenge 2: Problem 1 ****
 # Store the sum of num1 and num2 in a variable.
 # Print the sum.
-
+print(5 + 10)
 
 # **** Challenge 2: Problem 2 ****
 # Store the difference between sum (above) and 7 in a variable.
 # Print the difference.
-
+print(15 - 7)
 
 # **** Challenge 2: Problem 3 ****
 # Store the product of the difference and 3 in a variable.
 # Print the product.
+print(8 * 3)
 
 
 # **** Challenge 2: Problem 4 ****
 # Store the power of the product squared in a variable.
 # Print the power.
-
+print(24 ** 2)
 
 # **** Challenge 2: Problem 5 ****
 # Store the quotient of the power divided by 4 in a variable.
 # Print the quotient.
-
+print(576/4)
 
 # **** Challenge 2: Problem 6 ****
 # Store the remainder of the quotient divided by 2 in a variable.
 # Print the remainder.
-
+print(576%4)
 
 # **** Upchallenge! ****
 # Given a variable that stores a number, print the values that come right before and right after.
 # For example, given the number 4, the program should print 3 and 5. 
 # Your code should work even if the value of the variable is changed.
 
-num = 9
+num = 3
+print({num - 1})
+print({num + 1})
 
 # -------------------------------------------- 
